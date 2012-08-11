6b7fbb39c16932512faf6c950e2456519790459a
unknown <Carole McCooby@.(none)>
2012-08-11 17:16:07 +0100

    add a welcome page

---
+++ MediaReviews/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MediaReviews.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        // GET: /Home/Welcome
+        public ViewResult Welcome()
+        {
+            // A method in the controller gets the data for the page and decides what kind of page
+
+            // create a class that has the data for the page            
+
+            // create an instance of that class and set the data
+            WelcomeData data = new WelcomeData();
+            data.Message = "Welcome";
+
+            // pass the data to the view and return it
+            return View(data);
+
+        }
+    }
+
+    public class WelcomeData
+    {
+        public string Message;
+    }
+
+}
---
+++ MediaReviews/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MediaReviews.MvcApplication" Language="C#" %>
