6019c3ca563fe091eaf4358d6726240a304553ad
Carole Reichenbach <carole_reichenbach@hotmail.com>
2012-08-19 20:41:39 +0100

    Set up dummy data for the home page

--- MediaReviews/Controllers/HomeController.cs
+++ MediaReviews/Controllers/HomeController.cs
@@ -21,17 +21,38 @@ namespace MediaReviews.Controllers
 
             // create an instance of that class and set the data
             WelcomeData data = new WelcomeData();
-            data.Message = "Welcome";
+            
+            // create 5 reviews
+            ReviewInformation review1 = new ReviewInformation{Type = "Book", Name = "fifty shades of grey", WhenReviewed = new DateTime(2012,08,19), Rating = 3, Review = "i loved it"};
+            ReviewInformation review2 = new ReviewInformation{Type = "song", Name = "love song for no one", WhenReviewed = new DateTime(2012,08,19), Rating = 3, Review = "i loved it"};
+            ReviewInformation review3 = new ReviewInformation{Type = "movie", Name = "titanic", WhenReviewed = new DateTime(2012,08,19), Rating = 3, Review = "i loved it"};
 
-            // pass the data to the view and return it
+            var reviewsList = new List<ReviewInformation>();
+            reviewsList.Add(review1);
+            reviewsList.Add(review2);
+            reviewsList.Add(review3);
+
+            data.RecentReviews = reviewsList;
+
             return View(data);
-
         }
     }
 
     public class WelcomeData
     {
-        public string Message;
+        public IEnumerable<ReviewInformation> RecentReviews { get; set; }
     }
 
+    public class ReviewInformation
+    {
+        public string Type { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime WhenReviewed { get; set; }
+
+        public int Rating { get; set; }
+
+        public string Review { get; set; }
+    }
 }
